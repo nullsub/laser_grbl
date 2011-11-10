6073fd66bcd56ce69b5457906ee455470b439d66
Stefan Hechenberger <stefan@nortd.com>
2011-11-10 14:58:50 +0100

    flash.py windows tool path added

--- flash.py
+++ flash.py
@@ -21,12 +21,22 @@ PORT = "/dev/tty.usbmodem621"
 #     The default here is what you use if you are on OSX with
 #     the Arduino IDE installed.
 
-AVRDUDEAPP = "/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin/avrdude"
-AVRGCCAPP = "/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin/avr-gcc"
+#OSX
+AVRDUDEAPP    = "/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin/avrdude"
+AVRGCCAPP     = "/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin/avr-gcc"
 AVROBJCOPYAPP = "/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin/avr-objcopy"
-AVRSIZEAPP = "/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin/avr-size"
+AVRSIZEAPP    = "/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin/avr-size"
 AVROBJDUMPAPP = "/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin/avr-objdump"
 AVRDUDECONFIG = "/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/etc/avrdude.conf"
+
+# Windows
+# AVRDUDEAPP    = "C:\Program Files\arduino-0022\hardware\tools\avr\bin\avrdude"
+# AVRGCCAPP     = "C:\Program Files\arduino-0022\hardware\tools\avr\bin\avr-gcc"
+# AVROBJCOPYAPP = "C:\Program Files\arduino-0022\hardware\tools\avr\bin\avr-objcopy"
+# AVRSIZEAPP    = "C:\Program Files\arduino-0022\hardware\tools\avr\bin\avr-size"
+# AVROBJDUMPAPP = "C:\Program Files\arduino-0022\hardware\tools\avr\bin\avr-objdump"
+# AVRDUDECONFIG = "C:\Program Files\arduino-0022\hardware\tools\avr\etc\avrdude.conf"
+
 
 # 3.) Compile LasaurGrbl and load it to an Arduino Uno via USB
 #     In the Teminal from the location of this flash.py type: 
@@ -59,9 +69,7 @@ os.system('%(objcopy)s -j .text -j .data -O ihex main.elf %(product)s.hex' % {'o
 
 os.system('%(size)s *.hex *.elf *.o' % {'size':AVRSIZEAPP})
 
-ret = raw_input('Do you want to upload the firmware to the Arduino? (Y/N default=N): ')
-if ret == 'Y' or ret == 'y':
-  os.system('%(dude)s -c %(programmer)s -b %(bps)s -P %(port)s -p %(device)s -C %(dudeconf)s -B 10 -F -U flash:w:%(product)s.hex:i' % {'dude':AVRDUDEAPP, 'programmer':PROGRAMMER, 'bps':BITRATE, 'port':PORT, 'device':DEVICE, 'dudeconf':AVRDUDECONFIG, 'product':BUILDNAME})
+os.system('%(dude)s -c %(programmer)s -b %(bps)s -P %(port)s -p %(device)s -C %(dudeconf)s -B 10 -F -U flash:w:%(product)s.hex:i' % {'dude':AVRDUDEAPP, 'programmer':PROGRAMMER, 'bps':BITRATE, 'port':PORT, 'device':DEVICE, 'dudeconf':AVRDUDECONFIG, 'product':BUILDNAME})
 
 # fuse setting taken over from Makefile for reference
 #os.system('%(dude)s -U hfuse:w:0xd2:m -U lfuse:w:0xff:m' % {'dude':AVRDUDEAPP})
