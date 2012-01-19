3f67f495a4b23b3e4e182c23bfc295d508050917
Stefan Hechenberger <stefan@nortd.com>
2012-01-19 22:28:12 +0100

    version updated to v12.01b

--- settings.h
+++ settings.h
@@ -26,7 +26,7 @@
 #include <math.h>
 #include <inttypes.h>
 
-#define GRBL_VERSION "12.01a"
+#define GRBL_VERSION "12.01b"
 
 // Version of the EEPROM data. Will be used to migrate existing data from older versions of Grbl
 // when firmware is upgraded. Always stored in byte 0 of eeprom
